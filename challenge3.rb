9a7e94eafbc14810081179526255c3e05f49f08a
Sherif Abushadi <sherif@amgando.org>
2014-07-18 11:46:30 -0700

    algo challenge: debuggin insertion sort

---
+++ challenge3.rb
@@ -0,0 +1,6 @@
+# http://www.sorting-algorithms.com/insertion-sort
+# for i = 2:n,
+#     for (k = i; k > 1 and a[k] < a[k-1]; k--)
+#         swap a[k,k-1]
+#     → invariant: a[1..i] is sorted
+# end
