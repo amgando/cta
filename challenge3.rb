4177cd83ff217e4a37ae3efe0468b4eb30b7c0f3
Sherif Abushadi <sherif@amgando.org>
2014-07-18 11:49:28 -0700

    start point for challenge 3

--- challenge3.rb
+++ challenge3.rb
@@ -4,3 +4,22 @@
 #         swap a[k,k-1]
 #     → invariant: a[1..i] is sorted
 # end
+
+def insertion_sort(a)
+
+  # for i = 2:n,
+  (2..a.length).each do |i|
+    k = i
+    # for (k = i; k > 1 and a[k] < a[k-1]; k--)
+    while k > 1 && a[k] < a[k-1]
+      # swap a[k,k-1]
+      a[k], a[k-1] = a[k-1], a[k]
+      k -= 1
+    end
+  end
+
+  return a
+end
+
+data = (1..100).to_a.sample(50)
+p insertion_sort(data)
