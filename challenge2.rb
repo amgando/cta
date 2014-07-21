7298c382d23c213994cfd1521a152a608f1050fd
Sherif Abushadi <sherif@amgando.org>
2014-07-21 09:51:16 -0700

    start point for challenge 2

---
+++ challenge2.rb
@@ -0,0 +1,55 @@
+class Orange
+  attr_reader :circumference
+
+  def initialize
+    @circumference = rand(1..12)
+  end
+
+end
+
+class OrangeTree
+  attr_accessor :age, :fruit
+  attr_reader :max_age, :fruit_bearing_age
+
+  def initialize(args = {})
+    @age = args.fetch(:age, 0)
+    @fruit = []
+    @max_age = args.fetch(:max_age, 30)
+    @fruit_bearing_age = args.fetch(:fruit_bearing_age, 5)
+  end
+
+  def alive?
+    age < max_age
+  end
+
+  def age!
+    age += 1
+  end
+
+  def grow_fruit!
+    fruit << Orange.new if alive? && fruit_bearing?
+  end
+
+  def pick_fruit!
+    "You picked an orange that's #{fruit.shift.circumference} inches around!"
+  end
+
+  def fruit_bearing?
+    age >= fruit_bearing_age
+  end
+
+end
+
+#Run this code. It should all work, except for when we call "tree.age!"
+
+#Read the error. Why is "age" nil? In the "age!" method, change "age" to "@age" or "self.age". Why does it work now?
+
+#Why does referencing "age" work inside the "alive?" method, but not the "age!" method?
+
+tree = OrangeTree.new({age: 6})
+
+3.times { tree.grow_fruit! }
+p tree.pick_fruit!
+p tree.alive?
+p tree.age
+p tree.age!
