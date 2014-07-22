be1795e3a5f0e5d9e28f1aa90415cc0a2983682f
Sherif Abushadi <sherif@amgando.org>
2014-07-22 15:26:32 -0700

    updated challenge 3 start point and instructions

--- challenge3.rb
+++ challenge3.rb
@@ -1,4 +1,9 @@
+# this program has an intermittent bug.
+# sometimes the tests pass, sometimes they fail.
+# make whatever changes you want to make all the tests pass
+
 # http://www.sorting-algorithms.com/insertion-sort
+
 # for i = 2:n,
 #     for (k = i; k > 1 and a[k] < a[k-1]; k--)
 #         swap a[k,k-1]
@@ -6,20 +11,20 @@
 # end
 
 def insertion_sort(a)
-
-  # for i = 2:n,
-  (2..a.length).each do |i|
+  (2...a.length).each do |i|
     k = i
-    # for (k = i; k > 1 and a[k] < a[k-1]; k--)
     while k > 1 && a[k] < a[k-1]
-      # swap a[k,k-1]
       a[k], a[k-1] = a[k-1], a[k]
       k -= 1
     end
   end
-
-  return a
+  a
 end
 
-data = (1..100).to_a.sample(50)
-p insertion_sort(data)
+# -----------------------------
+
+10.times do
+  data = (1..100).to_a.sample(10)
+  print "%-50s" % [insertion_sort(data)]
+  puts insertion_sort(data) == data.sort  # test against internal sort
+end
