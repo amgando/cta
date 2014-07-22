29dbadd786a43935ce76312774080387eaa64749
Sherif Abushadi <sherif@amgando.org>
2014-07-22 14:36:21 -0700

    algo challenge: debugging selection sort

---
+++ challenge4.rb
@@ -0,0 +1,9 @@
+# http://www.sorting-algorithms.com/selection-sort
+
+# for i = 1:n,
+#     k = i
+#     for j = i+1:n, if a[j] < a[k], k = j
+#     → invariant: a[k] smallest of a[i..n]
+#     swap a[i,k]
+#     → invariant: a[1..i] in final position
+# end
