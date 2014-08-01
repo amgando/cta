53330b358a2f3b620abed680e3c1719f3d6bcebd
Apprentice <apprentice@dbcadam.local>
2014-07-31 18:13:52 -0700

    End of session

--- challenge2.rb
+++ challenge2.rb
@@ -23,7 +23,7 @@ class OrangeTree
   end
 
   def age!
-    age += 1
+    @age += 1
   end
 
   def grow_fruit!
@@ -43,6 +43,7 @@ end
 tree = OrangeTree.new({age: 6})
 
 3.times { tree.grow_fruit! }
+
 p tree.pick_fruit!
 p tree.alive?
 p tree.age
--- challenge4.rb
+++ challenge4.rb
@@ -11,10 +11,17 @@
 # end
 
 def insertion_sort(a)
-  (2...a.length).each do |i|
+  # indexes from third to last ... weird
+  (2...a.length).
+  # for each index
+  each do |i|
+    # dont get it
     k = i
-    while k > 1 && a[k] < a[k-1]
+    # ... don't understand
+    while k > 0 && a[k] < a[k-1]
+      # swap
       a[k], a[k-1] = a[k-1], a[k]
+      # mutates k
       k -= 1
     end
   end
@@ -23,8 +30,10 @@ end
 
 # -----------------------------
 
-10.times do
-  data = (1..100).to_a.sample(10)
-  print "%-50s" % [insertion_sort(data)]
-  puts insertion_sort(data) == data.sort  # test against internal sort
-end
+p insertion_sort([2,1])
+
+# 10.times do
+#   data = (1..100).to_a.sample(10)
+#   print "%-50s" % [insertion_sort(data)]
+#   puts insertion_sort(data) == data.sort  # test against internal sort
+# end
